e692d74b8dfbd754a667fe1d65e400b5e82e6c43
Michael Luu <67599521+kemonprogrammer@users.noreply.github.com>
2026-02-10 03:30:49 +0100

    Add setup pass script

--- scripts/setup-pass.sh
+++ scripts/setup-pass.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-# check if python3 installed
 
 sudo yum install -y \
   pass \
-  pass-import
 
+GPG_KEY_PATH="$HOME/key.asc"
+GIT_REPO="git@github.com:kemonprogrammer/passwordstore.git"
 
-# if ! command -v pimport &> /dev/null; then
-#   echo "installing pass-import"
-#   wget https://github.com/roddhjav/pass-import/releases/download/v3.5/pass-import-3.5.tar.gz
-#   tar xzf pass-import-3.5.tar.gz
-#   cd pass-import-3.5
-#   sudo pip install .
-# else
-#   echo "pass-import is already installed system-wide."
-# fi
+echo "⚙️ Setting up password store..."
+
+# Import and silence
+gpg --import "$GPG_KEY_PATH" 
+
+# Get Fingerprint
+FINGERPRINT=$(gpg --with-colons --import-options show-only --import "$GPG_KEY_PATH" | awk -F: '$1 == "fpr" {print $10; exit}')
+
+# Set Trust and silence
+echo -e "trust\n5\ny\n" | gpg --command-fd 0 --edit-key "$FINGERPRINT" >/dev/null 2>&1
+
+# Clone and Init
+if [ ! -d "$HOME/.password-store" ]; then
+    echo "Cloning git"
+    git clone "$GIT_REPO" "$HOME/.password-store" >/dev/null 2>&1
+fi
+
+pass init "$FINGERPRINT" 
+
+echo "✅ Setup Pass Done!"
