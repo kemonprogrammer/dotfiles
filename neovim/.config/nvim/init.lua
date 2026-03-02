8ed91b5499c72090df57ba638e494a5c550a2228
Michael Luu <67599521+kemonprogrammer@users.noreply.github.com>
2026-03-02 02:39:41 +0100

    Add lua lsp, git signs, command autocompletion

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,5 +1,9 @@
--- Stop jumping from warnings
+-- Stop jumping left gutter from warnings
 vim.opt.signcolumn = "yes"
+
+-- Auto suggestions command mode
+vim.opt.wildmenu = true
+vim.opt.wildoptions = "pum"
 
 -- Needs to be done before plugins
 vim.g.mapleader = " "
@@ -30,21 +34,26 @@ Plug 'tpope/vim-commentary'
 -- Plug 'vim-airline/vim-airline'
 
 -- Fuzzy finder
-Plug ('junegunn/fzf', { ['do'] = vim.fn['fzf#install()'] })
+Plug ('junegunn/fzf', { ['do'] = './install --all' })
 Plug 'junegunn/fzf.vim'
 
 -- Highlighting
 Plug ('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 
--- Auto-pairs 
--- Plug 'jiangmiao/auto-pairs' 
+-- Auto-pairs
+-- Plug 'jiangmiao/auto-pairs'
 Plug 'Raimondi/delimitMate'
+
+-- Git
+Plug 'tpope/vim-fugitive'
+Plug 'lewis6991/gitsigns.nvim'
 
 -- Latex
 Plug 'lervag/vimtex'
 Plug 'nvim-telescope/telescope-bibtex.nvim'
 
 -- LSP
+Plug 'mason-org/mason.nvim'
 Plug 'neovim/nvim-lspconfig'
 
 -- Completion manager
@@ -52,6 +61,8 @@ Plug 'hrsh7th/nvim-cmp'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
 Plug 'L3MON4D3/LuaSnip'        -- Snippet engine (required for most cmp setups)
+
+Plug 'gelguy/wilder.nvim'
 -- Plug 'ncm2/ncm2'
 -- Plug 'roxma/nvim-yarp'
 
@@ -93,17 +104,17 @@ vim.opt.shiftwidth = 2
 -- #TODO
 -- ----  File Explorer  ----
 -- --- NERDTree ---
--- 
+--
 -- -- Start NERDTree, unless a file or session is specified, eg. vim -S session_file.vim.
 -- autocmd StdinReadPre * let s:std_in=1
 -- autocmd VimEnter * if argc() == 0 && !exists('s:std_in') && v:this_session == '' | NERDTree | endif
--- 
+--
 -- -- Exit Vim if NERDTree is the only window remaining in the only tab.
 -- -- autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
--- 
+--
 -- -- Close the tab if NERDTree is the only window remaining in it.
 -- autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
--- 
+--
 -- -- If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
 -- autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
 --     \ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
@@ -138,7 +149,7 @@ vim.api.nvim_set_keymap('i', '<C-c>', '<Esc>', { noremap = true, silent = true }
 nmap('<C-S>', ':update<CR>')
 --vnoremap <silent> <C-S> <ESC>:update<CR>gv
 vmap('<C-S>', '<ESC>:update<CR>gv')
----- TODO: try neovims beta feature CTRL+S 
+---- TODO: try neovims beta feature CTRL+S
 --inoremap <silent> <C-S> <C-O>:update<CR>
 --imap('<C-s>', '<C-O>:update<CR>')
 
@@ -148,10 +159,13 @@ vmap('<C-S>', '<ESC>:update<CR>gv')
 --nnoremap <leader>et :NERDTreeToggle<CR>
 ---- nmap <Leader>fzf :Files<CR>
 --nmap <Leader>f :GFiles<CR>
---nmap <Leader>pc :PlugClean<CR>
---nmap <Leader>pi :PlugInstall<CR>
---nmap <Leader>pu :PlugUpdate<CR>
---nmap <Leader>so :source $MYVIMRC<CR>
+vim.keymap.set('n', '<leader>pc', '<cmd>PlugClean<CR>', { desc = 'Plug Clean' })
+vim.keymap.set('n', '<leader>pi', '<cmd>PlugInstall<CR>', { desc = 'Plug Install' })
+vim.keymap.set('n', '<leader>pu', '<cmd>PlugUpdate<CR>', { desc = 'Plug Update' })
+vim.keymap.set('n', '<leader>so', function()
+  vim.cmd('source ' .. vim.env.MYVIMRC)
+  print("Neovim config sourced")
+end, { desc = 'Source init.lua' })
 --
 ---- Autocenter
 ----nnoremap G Gzz
@@ -234,8 +248,8 @@ local builtin = require('telescope.builtin')
 
 -- The "Standard" Neovim style (highly recommended)
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Search Text in Files' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find file' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Search text in files' })
 
 
 -- Latex
@@ -243,7 +257,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Search Text in Fi
 if is_wsl then
     -- SumatraPDF path (WSL path to the Windows .exe)
     local sumatra_path = '/mnt/c/Users/MichaelLuu/AppData/Local/SumatraPDF/SumatraPDF.exe'
-    
+
     vim.g.vimtex_view_general_viewer = sumatra_path
     vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
 end
@@ -268,9 +282,36 @@ vim.lsp.config['texlab'] = {
 }
 
 
+-- LSP
+
+require("mason").setup()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+vim.lsp.config['lua-language-server'] = {
+  cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/lua-language-server") },
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = { version = 'LuaJIT' },
+      diagnostics = {
+        globals = { 'vim' }, -- Fix the "Undefined global 'vim'" warning
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = { enable = false },
+    },
+  },
+}
+
+vim.lsp.enable('lua-language-server')
+
+
 -- Completion manager
 
-local cmp = require'cmp'
+local cmp = require("cmp")
 
 cmp.setup({
   snippet = {
@@ -281,8 +322,9 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-e>'] = cmp.mapping.abort(),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), 
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
@@ -292,3 +334,98 @@ cmp.setup({
 })
 
 
+-- Completion for command line
+local wilder = require('wilder')
+wilder.setup({modes = {':'}})
+
+wilder.set_option('renderer', wilder.popupmenu_renderer(
+  -- wilder.popupmenu_border_theme({
+  --   -- 'highlighter' apply colors to the matched text
+  --   highlighter = wilder.basic_highlighter(),
+
+  --   -- Set the highlights for the menu colors
+  --   highlights = {
+  --     border = 'Normal', -- border color
+  --   },
+
+  --   -- can be 'single', 'double', 'rounded' or 'solid'
+  --   border = '',
+
+  --   max_height = '20%',
+
+  --   -- min_width = '100%',
+
+  --   -- Use the default prompt (usually at the bottom)
+  --   reverse = false,
+  -- })
+))
+
+
+-- Git
+
+-- Git signs
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gitsigns = require('gitsigns')
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    -- Actions
+    map('n', '<leader>hs', gitsigns.stage_hunk)
+    map('n', '<leader>hr', gitsigns.reset_hunk)
+
+    map('v', '<leader>hs', function()
+      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('v', '<leader>hr', function()
+      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('n', '<leader>hS', gitsigns.stage_buffer)
+    map('n', '<leader>hR', gitsigns.reset_buffer)
+    map('n', '<leader>hp', gitsigns.preview_hunk)
+    map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+
+    map('n', '<leader>hb', function()
+      gitsigns.blame_line({ full = true })
+    end)
+
+    map('n', '<leader>hd', gitsigns.diffthis)
+
+    map('n', '<leader>hD', function()
+      gitsigns.diffthis('~')
+    end)
+
+    map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
+    map('n', '<leader>hq', gitsigns.setqflist)
+
+    -- Toggles
+    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+    map('n', '<leader>tw', gitsigns.toggle_word_diff)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', gitsigns.select_hunk)
+  end
+}
